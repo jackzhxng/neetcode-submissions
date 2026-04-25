cbce50a6bed565415b739e0de223d14dcdbbab3d
Jack <32371937+jackzhxng@users.noreply.github.com>
2026-04-24 19:30:06 -0700

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        """
+        Time: 15 mins
+
+        Naive bottom up DP passed but placed ~90%, added `furthest` optimization to reduce number of iterations
+        """
+        dp = [None for _ in range(len(nums))]
+        dp[0] = 0
+        furthest = 0
+        for i, num in enumerate(nums):
+            if dp[i] is None:
+                return -1 # Shouldn't happen since it's guaranteed you can reach nums[n - 1].
+            for dest in range(max(furthest, i + 1), min(len(nums), i + 1 + num)):
+                if dp[dest] is None:
+                    dp[dest] = dp[i] + 1
+                else:
+                    dp[dest] = min(dp[dest], dp[i] + 1)
+                furthest = max(furthest, dest)
+        return dp[-1]
